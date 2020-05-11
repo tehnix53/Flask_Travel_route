a7a9ae17bce2de04318c76c31510e2aaa6cdcad4
Alexander <44552657+tehnix53@users.noreply.github.com>
2020-05-11 07:35:31 +0200

    Update app.py

--- app.py
+++ app.py
@@ -25,4 +25,8 @@ def departure_start(id):
                            filtered=filtered, price=price, nights=nights)
 
 
-app.run('127.0.0.1', 8000, debug=True)
+#app.run('127.0.0.1', 8000, debug=True)
+
+if __name__ == '__main__':
+    app.run()
+
